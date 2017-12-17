4be8e5bc4325773696f80df22e71cdc4b4dee26a
Philipp Stadler <hello@phstadler.com>
2017-12-17 11:52:24 +0100

    Started adding notes on sampling

--- src/geom/surf/sampling.rs
+++ src/geom/surf/sampling.rs
@@ -2,9 +2,12 @@
 use ::geom::tri::Triangle;
 use ::geom::vtx::Vertex;
 
+
 fn sample<I, V>(triangles: I)
     where I : IntoIterator<Item = Triangle<V>>,
         V : Vertex
 {
-
+    // 1. make active list of all triangles (logarithmicly binned by area)
+    // 2. throw darts
+    // 2.1
 }
